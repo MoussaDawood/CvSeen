2874c6ca0dabb530f9239f925cba505a3fe35f55
MoussaDawood <moussa.dawood1@gmail.com>
2024-02-22 14:45:12 +0200

    On branch master Changes to be committed: 	new file:   __pycache__/main.cpython-310.pyc 	new file:   getchatid.py 	modified:   main.py

---
+++ getchatid.py
@@ -0,0 +1,31 @@
+import requests
+
+# get it from @BotFather
+TelegramToken = 'WILL_NOT_PUBLISH_MY_TOKEN_OF_COURSE'
+
+def get_chat_id():
+    response = requests.get(f'https://api.telegram.org/bot{TelegramToken}/getUpdates')
+    data = response.json()
+    if data['ok']:
+        # Get the latest message
+        latest_message = data['result'][-1]
+        # Get the chat ID from the latest message
+        chat_id = latest_message['message']['chat']['id']
+        # print(latest_message,"666")
+
+        return chat_id
+    else:
+        # print("Error occurred while fetching updates:", data['description'])
+        return None
+
+if __name__ == "__main__":
+    # send msg to your telegram bot first ex: /start
+    chat_id = get_chat_id()
+    if chat_id:
+        print("Chat ID:", chat_id)
+    else:
+        print("Failed to retrieve the chat ID.")
+
+
+
+
--- main.py
+++ main.py
@@ -1,38 +1,60 @@
 from fastapi import FastAPI
 from telegram import Bot
+import requests
+import uvicorn
+
+# get it from @BotFather
+bot_token = '7089629432:AAEX9ts73EvYmte66oHMh2v52xvyok2XYCw'
+# get it from getchatid.py
+chat_id = '566639260'
 
 
-Telegrm_Token = 'YOUR_TOKEN'
-# Replace 'CHAT_ID' with the chat ID of the recipient
-# You can obtain it by sending a message to your bot and then fetching it from the received message object
-# Alternatively, you can use your own chat ID for testing purposes
-Telegram_group = 'YOUR_CHAT_ID'
 app = FastAPI()
 
 
+def send_message(token, chat_id, message):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    response = requests.post(url, params=params)
+    if response.status_code == 200:
+        print("Message sent successfully")
+    else:
+        print(f"Failed to send message. Error: {response.text}")
 
-@app.get('/')
-def root():
-    pass
 
-
-def send_message(chat_id, message):
-    bot = Bot(token=Telegrm_Token)
-    bot.send_message(chat_id=chat_id, text=message)
-
-
-def get_cv_title():
+def get_cv_title(id):
     # TODO:
     return 'CV_TITLE'
 
 
-
+def get_original_url(id):
+    return "https://google.com"
+    
+    
+def redirect_to_original(original_url):
+    pass
+    
+    
 @app.get('/')
 def short_url_request(id:str):    
+    
+    # send message on telegram 
     cv_title = get_cv_title(id)
     message_text = f"Cv with title : {cv_title} is opened"
-    send_message(Telegram_group, message_text)
-    
+    send_message(bot_token,chat_id, message_text)
 
-if __name__ == "__main__":
-    pass
+    
+    # 301 to the actual location 
+    original_url = get_original_url(id)
+    redirect_to_original(original_url)
+
+
+    # save click in sql database
+
+
+
+# if __name__ == "__main__":
+    # uvicorn.run("main:app",host="127.0.0.1",port=8000)
